e86d3ed965fde08c1ce5fcce5e094e907707e574
NewbieAnderson <engineeringsong@gmail.com>
2023-05-07 23:20:26 +0900

    added file pointer(steam) test code

--- Makefile
+++ Makefile
@@ -5,3 +5,4 @@ src/file:
 	gcc -o ./bin/io ./file/io.c
 	gcc -o ./bin/offset ./file/offset.c
 	gcc -o ./bin/io_redirection ./file/io_redirection.c
+	gcc -o ./bin/fp ./file/fp.c
---
+++ file/fp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    FILE *fp = fopen("./fp_test.txt", "w");
+    if (fp == NULL) {
+        perror("failed to open file ");
+        exit(1);
+    }
+    if (fclose(fp) == -1)
+        perror("failed to close file pointer ");
+    return 0;
+}
