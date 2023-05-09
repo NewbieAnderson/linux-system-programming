b9a7c12c2868be861b5a887e3a70c05a4e2e98c9
NewbieAnderson <engineeringsong@gmail.com>
2023-05-09 15:39:37 +0900

    added fp_io.c

--- Makefile
+++ Makefile
@@ -6,3 +6,4 @@ src/file:
 	gcc -o ./bin/fd_offset ./file/fd_offset.c
 	gcc -o ./bin/fd_io_redirection ./file/fd_io_redirection.c
 	gcc -o ./bin/fp ./file/fp.c
+	gcc -o ./bin/fp_io ./file/fp_io.c
---
+++ file/fp_io.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    FILE *rfp = fopen("./fp_test.txt", "r");
+    FILE *wfp = fopen("./fp_test.txt", "a");
+    char buf[256] = { 0, };
+    char *addr = NULL;
+    int ch = '\0';
+    int i;
+    if (rfp == NULL) {
+        perror("failed to open file as read mode ");
+        exit(1);
+    }
+    if (wfp == NULL) {
+        perror("failed to open file as write mode ");
+        exit(1);
+    }
+    printf("reading...\n");
+    while ((ch = fgetc(rfp)) != EOF)
+        putc(ch, stdout);
+    putchar('\n');
+    puts("Hello puts()!");
+    if (fputs("Hello fputs()!\n", wfp) < 0)
+        perror("failed to write in wfp by using fputs() ");
+    addr = fgets(buf, 256, rfp);
+    printf("addr : %p\n"
+            "buf : %p\n"
+            "&buf : %p\n", addr, buf, &buf);
+    if (addr < 0)
+        perror("failed to read by using fgets() ");
+    if (fclose(rfp) == -1)
+        perror("failed to close rfp ");
+    if (fclose(wfp) == -1)
+        perror("failed to close wfp ");
+    return 0;
+}
