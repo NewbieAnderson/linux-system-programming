58c00c928da4c64159287ab2781db0d4ee0c75bf
NewbieAnderson <engineeringsong@gmail.com>
2023-05-15 23:59:12 +0900

    fixed few codes

--- file/buf_mode.c
+++ file/buf_mode.c
@@ -12,7 +12,7 @@ int main(void)
     char buf1[BUFFER_SIZE] = { 0, };
     char buf2[BUFFER_SIZE] = { 0, };
     char buf3[BUFFER_SIZE] = { 0, };
-    clock_t start, end;
+    double start, end;
     if (fp1 == NULL) {
         perror("failed to open file ");
         exit(1);
@@ -28,18 +28,18 @@ int main(void)
     setvbuf(fp1, buf1, _IONBF, BUFFER_SIZE);
     setvbuf(fp2, buf2, _IOLBF, BUFFER_SIZE);
     setvbuf(fp3, buf3, _IOFBF, BUFFER_SIZE);
-    start = clock();
+    start = (double)clock();
     fputs("un-buffered___\n", fp1);
-    end = clock();
-    printf("execution time (no buffered) : %ld ms\n", (end - start) * 1000 / CLOCKS_PER_SEC);
-    start = clock();
+    end = (double)clock();
+    printf("execution time (no buffered) : %.2lfms\n", (end - start));
+    start = (double)clock();
     fputs("line-buffered_\n", fp2);
-    end = clock();
-    printf("execution time (line buffered) : %ld ms\n", (end - start) * 1000 / CLOCKS_PER_SEC);
-    start = clock();
+    end = (double)clock();
+    printf("execution time (line buffered) : %.2lfms\n", (end - start));
+    start = (double)clock();
     fputs("block-buffered\n", fp3);
-    end = clock();
-    printf("execution time (fully buffered) : %ld ms\n", (end - start) * 1000 / CLOCKS_PER_SEC);
+    end = (double)clock();
+    printf("execution time (fully buffered) : %.2lfms\n", (end - start));
     if (fclose(fp1) == -1)
         perror("failed to close fp1 ");
     if (fclose(fp2) == -1)
