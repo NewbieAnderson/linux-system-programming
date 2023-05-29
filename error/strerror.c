7a80b8971f9db96eb395d34e28629dba21ccdc4f
NewbieAnderson <engineeringsong@gmail.com>
2023-05-29 22:09:45 +0900

    changed example

--- error/strerror.c
+++ error/strerror.c
@@ -5,8 +5,8 @@
 
 int main(void)
 {
-    int err = 0;
-    for (err = 0; err < 10; ++err)
-        printf("error code %d : %s\n", err, strerror(err));
+    int _errno = 0; /* do not use "errno" as variable name */
+    for (_errno = 0; _errno < 10; ++_errno)
+        printf("errno %d : %s\n", _errno, strerror(_errno));
     return 0;
 }
